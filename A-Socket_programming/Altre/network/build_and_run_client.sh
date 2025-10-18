c91387c73d5385d7944de30d00536c05dd501764
filippo-bilardo <filippo.bilardo@tiscali.it>
2025-10-18 14:29:06 +0200

    update 4

--- A-Socket_programming/Altre/network/build_and_run_client.sh
+++
@@ -1,3 +0,0 @@
-cd /workspace/TPSIT_3/B-ES03-03-ServerWeb/
-javac network/TcpClient.java
-java network/TcpClient 
--- A-Socket_programming/Altre/network/build_and_run_server.sh
+++
@@ -1,3 +0,0 @@
-cd /workspace/TPSIT_3/B-ES03-03-ServerWeb/
-javac network/TcpServer.java
-java network/TcpServer 
